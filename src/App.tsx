ff22d75fc76d453945ceb93e30525ef366cfef69
José Carlos <etijcvivi@gmail.com>
2022-04-25 22:02:12 -0300

    Descrição da forma de importar o componente no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+// importa todos os componentes, poderia ser feito da seguinte forma:
+// import { Container } from './App.styles';
+// Dentro da constante App poderia ser chamado: <Container></Container>
+// isso facilita quando se tem muitos componentes em uma tela.
 import * as Componente from './App.styles';
 
 const App = () => {
   return (
     <Componente.Container>
-      
+      ...
     </Componente.Container>
   );
 }
