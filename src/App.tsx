fbe73805eb3155d48e6914a940e2456fa25d216b
José Carlos <etijcvivi@gmail.com>
2022-04-27 23:12:23 -0300

    Adicionando o primeiro Item na Lista

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,28 @@
 // Dentro da constante App poderia ser chamado: <Container></Container>
 // isso facilita quando se tem muitos componentes em uma tela.
 import * as Componente from './App.styles';
+import { useState } from 'react';
+import { Item } from './types/item';
 
 const App = () => {
+
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar o pão na padaria', done: false },
+    { id: 2, name: 'Comprar um bolo na padaria', done: false },
+  ]);
+
   return (
     <Componente.Container>
       <Componente.Area>
         <Componente.Header>
           Lista de Tarefas
         </Componente.Header>
+
+        {/* Adiciona nova tarefa */}
+        {list.map((item, index)=>(
+            <div>...</div>
+        ))}
+
       </Componente.Area>
     </Componente.Container>
   );
