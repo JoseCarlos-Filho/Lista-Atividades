07bfe80053c2a5d400593d20e53161c2ac2d80e9
José Carlos <etijcvivi@gmail.com>
2022-04-28 22:45:46 -0300

    Inserindo Input CheckBox e estilizando o label com as Tarefas.

--- src/components/ListItem/styles.ts
+++ src/components/ListItem/styles.ts
@@ -1,3 +1,27 @@
 import styled from 'styled-components';
 
-export const Container = styled.div ``;
+// Observação os elementos no caso Tags estão dentro do Container
+// A estilização esta sendo feita dentro do Container, Logo todas as tags dentro do Container
+// podem ser chamadas e estilizadas.
+export const Container = styled.div `
+    display: flex;
+    background-color: #20212C;
+    padding: 10px;
+    border-radius: 10px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    align-items: center;
+    box-shadow: 0 0 10px gray;
+
+    input { 
+        width: 25px;
+        height: 25px;
+        margin-right: 5px;
+    }
+
+    
+    label { 
+        color: #CCC;
+    }
+
+`;
