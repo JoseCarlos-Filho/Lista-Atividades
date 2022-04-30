4af5074fd8e6cee0d3d88ffdc94e309c6aa968d8
José Carlos <etijcvivi@gmail.com>
2022-04-29 21:19:21 -0300

    Adicionando marcação na tarefa selecionada como feito

--- src/components/ListItem/styles.ts
+++ src/components/ListItem/styles.ts
@@ -1,9 +1,19 @@
 import styled from 'styled-components';
 
+
+// Propriedade Booleana para manipulação do text-decoration na hora de selecionar a tarefa realizada ou não.
+type ContainerProps = {
+    done: boolean;
+}
+
 // Observação os elementos no caso Tags estão dentro do Container
 // A estilização esta sendo feita dentro do Container, Logo todas as tags dentro do Container
 // podem ser chamadas e estilizadas.
-export const Container = styled.div `
+
+// Propriedade { done } é passada dentro de uma função com seu respectivo tipo : ContainerProps
+export const Container = styled.div(({ done }: ContainerProps) =>( 
+
+`
     display: flex;
     background-color: #20212C;
     padding: 10px;
@@ -19,9 +29,10 @@ export const Container = styled.div `
         margin-right: 5px;
     }
 
-    
+
     label { 
         color: #CCC;
-    }
-
-`;
+        text-decoration: ${done ? 'line-through' : 'initial'};
+    }    
+`
+));
